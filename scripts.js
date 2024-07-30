a6aee803bf525a9e2913b66eacaff90bb0e78618
GuiBrumz <guibrumbatista@gmail.com>
2024-07-30 09:40:24 -0300

    aa

--- scripts.js
+++ scripts.js
@@ -48,6 +48,65 @@ document.getElementById('messageForm').addEventListener('submit', function(event
     }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var postagensSalvas = JSON.parse(localStorage.getItem('postagensSalvas')) || [];
+    var messagesDiv = document.getElementById('messages');
+ 
+    // Função para renderizar postagens
+    function renderPostagens() {
+        messagesDiv.innerHTML = '';
+ 
+        // Mostrar ou esconder o contêiner com base na presença de mensagens
+        if (postagensSalvas.length > 0) {
+            messagesDiv.style.display = 'block'; // Mostrar contêiner se houver mensagens
+        } else {
+            messagesDiv.style.display = 'none'; // Esconder contêiner se não houver mensagens
+        }
+ 
+        postagensSalvas.forEach(function(postagem, index) {
+            var messageItem = document.createElement('div');
+            messageItem.classList.add('message-item');
+ 
+            var p = document.createElement('p');
+            p.textContent = postagem;
+ 
+            var deleteButton = document.createElement('button');
+            deleteButton.classList.add('delete-button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', function() {
+                postagensSalvas.splice(index, 1);
+                localStorage.setItem('postagensSalvas', JSON.stringify(postagensSalvas));
+                renderPostagens();
+            });
+ 
+            messageItem.appendChild(p);
+            messageItem.appendChild(deleteButton);
+            messagesDiv.appendChild(messageItem);
+        });
+    }
+ 
+    // Renderizar postagens salvas ao carregar a página
+    renderPostagens();
+ 
+    // Adicionar evento de clique ao botão de comentar
+    document.getElementById('handleSubmit').addEventListener('click', function() {
+        var texto = document.getElementById('title').value;
+        if (texto) {
+            postagensSalvas.push(texto);
+            localStorage.setItem('postagensSalvas', JSON.stringify(postagensSalvas));
+            renderPostagens();
+            document.getElementById('title').value = ''; // Limpar o campo de entrada
+        }
+    });
+ 
+    // Adicionar evento de clique ao botão de cancelar (limpar postagens)
+    document.getElementById('handleClear').addEventListener('click', function() {
+        localStorage.removeItem('postagensSalvas');
+        postagensSalvas = 0;
+        renderPostagens();
+    });
+});
+
 function redirectToIndex() {
     window.location.href = "index.html";
 }
